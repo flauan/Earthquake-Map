03fe98d21e704f1826ef8404b902f0ab13aeab88
fervis lauan <fervis@hotmail.com>
2017-12-29 00:49:41 -0500

    Initial code

--- static/script.js
+++ static/script.js
@@ -1,7 +1,8 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+// var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
+//   "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
+var queryUrl="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
@@ -9,8 +10,8 @@ d3.json(queryUrl, function(data) {
   createFeatures(data.features);
 });
 
-function createFeatures(dumData) {
-    var earthquakeData=usgs_data.features;
+function createFeatures(earthquakeData) {
+    // var earthquakeData=usgs_data.features;
     var platesData=plates.features;
 
 
