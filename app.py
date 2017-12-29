f6a6fccda11197ac6732824ed109947aaecb0805
fervis lauan <fervis@hotmail.com>
2017-12-29 00:19:35 -0500

    Initial code

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, jsonify, redirect
+from flask import jsonify
+import csv
+import json
+import sqlite3 as sql
+import pandas as pd  
+# from __future__ import print_function
+import sys
+
+
+app = Flask(__name__)
+app.debug=True
+
+
+@app.route("/")
+def index():    
+    
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
